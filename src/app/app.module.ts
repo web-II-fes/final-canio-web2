6c3c99da08ed9fdbac08e22c95cc4ecc71048a5c
Sofia <caniosofia@gmail.com>
2020-12-15 18:58:28 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule  } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { CrearComponent } from './crear/crear/crear.component';
+import { MostrarComponent } from './item/mostrar/mostrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearComponent,
+    MostrarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clases/paquete.ts
@@ -0,0 +1,8 @@
+export class Paquete {
+    constructor(
+		public nombreRemit: string,
+		public direccionRemit: string,
+        public descripcion: string,
+        public fechaEntrega: Date
+    ){}
+}
